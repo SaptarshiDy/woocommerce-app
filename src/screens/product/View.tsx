21efbe9fea44839d484ab13f7a9ab364bfbd1923
Saptarshi Dey <saptarshi@meavita.in>
2024-02-08 00:20:57 +0530

    product view page update

--- src/screens/product/View.tsx
+++ src/screens/product/View.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView, FlatList, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, Image, ScrollView, FlatList, Dimensions, TouchableOpacity, ActivityIndicator, Pressable } from 'react-native';
 import { encode } from 'base-64';
 import Carousel from 'react-native-reanimated-carousel';
 import Product from '../../services/product';
@@ -24,51 +24,119 @@ const Landing = (props: any) => {
         product ?
             <View className='flex'>
 
-                <View>
-                    <FlatList
-                        horizontal={true}
-                        pagingEnabled={true}
-                        showsHorizontalScrollIndicator={false}
-                        data={product.images}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => {
-                            return (
-                                <View
-                                    style={{
-                                        width: width,
-                                        height: height / 2,
-                                    }}
-                                >
-                                    <Image
-                                        style={{
-                                            flex: 1,
-                                            width: null,
-                                            height: null,
-                                            resizeMode: 'cover',
-                                        }}
-                                        source={{
-                                            uri: item.src,
-                                        }}
-                                    />
-                                </View>
-                            )
-                        }}
-                    />
-                </View>
+                {/* Header */}
+                {/* <View className='flex flex-row bg-white w-full py-1'>
 
-                <View className='p-4'>
-                    <Text className='text-xl font-bold'>
-                        {product.name}
-                    </Text>
-                    <Text className='text-base'>
-                        {product.description.replace(/<[^>]*>?/gm, '')}
-                    </Text>
+                    <Pressable
+                        className='px-3 py-4 bg-slate-400 rounded flex-1 mx-3' 
+                        onPress={() => {}}
+                    >
+                        <Text className='text-center text-black'>Back</Text>
+                    </Pressable>
+                    <Pressable
+                        className='px-3 py-4 bg-slate-400 rounded flex-1 mx-3' 
+                        onPress={() => {}}
+                    >
+                        <Text className='text-center text-black'>Back</Text>
+                    </Pressable>
+
+                </View> */}
+                {/* End */}
+
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        flex: 1,
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        flexShrink: 1,
+                        paddingBottom: 60,
+                    }}
+                    className='flex-row flex-wrap'
+                >
+                    <View>
+                        <FlatList
+                            horizontal={true}
+                            pagingEnabled={true}
+                            showsHorizontalScrollIndicator={false}
+                            data={product.images}
+                            keyExtractor={item => item.id}
+                            renderItem={({ item }) => {
+                                return (
+                                    <View
+                                        style={{
+                                            width: width,
+                                            height: height / 2,
+                                        }}
+                                    >
+                                        <Image
+                                            style={{
+                                                flex: 1,
+                                                width: null,
+                                                height: null,
+                                                resizeMode: 'cover',
+                                            }}
+                                            source={{
+                                                uri: item.src,
+                                            }}
+                                        />
+                                    </View>
+                                )
+                            }}
+                        />
+                    </View>
+
+                    <View className='p-4'>
+                        <Text className='text-xl font-bold'>
+                            {product.name}
+                        </Text>
+                        <Text className='text-base'>
+                            {product.description.replace(/<[^>]*>?/gm, '')}
+                        </Text>
+                    </View>
+                    <View className='p-4'>
+                        <Text className='text-xl font-bold'>
+                            {product.name}
+                        </Text>
+                        <Text className='text-base'>
+                            {product.description.replace(/<[^>]*>?/gm, '')}
+                        </Text>
+                    </View>
+                    <View className='p-4'>
+                        <Text className='text-xl font-bold'>
+                            {product.name}
+                        </Text>
+                        <Text className='text-base'>
+                            {product.description.replace(/<[^>]*>?/gm, '')}
+                        </Text>
+                    </View>
+                </ScrollView>
+
+                {/* Footer */}
+                <View className='absolute left-0 right-0 bottom-0 flex flex-row bg-white w-full py-1'>
+                    
+                    <Pressable
+                        className='px-3 py-4 bg-slate-400 rounded flex-1 mx-3' 
+                        onPress={() => {}}
+                    >
+                        <Text className='text-center'>Add To Card</Text>
+                    </Pressable>
+                    
+                    <Pressable
+                        className='px-3 py-4 bg-[#7b51ad] rounded flex-1 mx-3' 
+                        onPress={() => {}}
+                    >
+                        <Text className='text-white text-center'>Buy Now</Text>
+                    </Pressable>
+
                 </View>
+                {/* End */}
 
             </View>
             :
-            <View>
-                <Text>Loading</Text>
+            <View className="flex h-5/6 justify-center items-center bg-white">
+                <ActivityIndicator size="large" color="#8359b5" />
             </View>
     );
 };
